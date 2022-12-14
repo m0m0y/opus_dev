ea776b146bdaef88014c9b812d283b201a38ce6e
unknown <rivera.jerome.dc.1525@gmail.com>
2022-12-14 11:30:09 +0800

    jeromeupdate

--- test-preparation.php
+++ test-preparation.php
@@ -23,7 +23,7 @@ $testPreparation = $testPreparation->getContent();
     <div class="container-fluid primary-bg">
         <div class="row">
             <div class="col-md-6 p-4 title-size">
-                <h1 class="m-5 px-5 fw-bold">Standardized test preparation helps students reach their goals.</h1>
+                <h1 class="m-5 px-5 fw-bold">Standardized Test Preparation helps students reach their goals.</h1>
                 <h4 class="px-5 my-5 lh-base">With Opus Academy’s test preparation programs, students will sharpen their skills to reach or even exceed their goals. Our students have seen great improvement in their scores, helping them to achieve their private school and/or college admission goals. We offer comprehensive standardized test preparation programs for the following exams, tailored for your student’s individual needs.</h4>
                 <center><a href ="#we-help-students" class="mx-5"><i class="bi bi-chevron-compact-down mx-auto"></i></a></center>
             </div>
